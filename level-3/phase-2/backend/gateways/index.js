e663f48808b3d5169bdeb6c8055622000013c707
Akash Singh <akashsingh854469@gmail.com>
2026-06-29 22:22:56 +0530

    Horizontail scaling done

--- level-3/phase-2/backend/gateways/index.js
+++ level-3/phase-2/backend/gateways/index.js
@@ -8,13 +8,13 @@ const app = express();
 const PORT = process.env.PORT || 8001;  
 
 app.get("/", (req, res) => {
-  res.send("Hello from Backend or Gateway");
+  res.send(`Hello from ${process.env.SERVER_NAME}`);
 });
 
 
-app.use("/auth", proxy("http://localhost:8001/"));
-app.use("/order", proxy("http://localhost:8002/"));
-app.use("/product", proxy("http://localhost:8003/"));
+app.use("/auth", proxy("http://auth-service:8001/"));
+app.use("/order", proxy("http://order-service:8002/"));
+app.use("/product", proxy("http://product-service:8003/"));
 
 
 app.listen(PORT, () => {
